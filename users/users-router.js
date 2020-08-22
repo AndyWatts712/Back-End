5489e1a39674bcb16caf7c35916aed30275fcc12
Natalia <nataliamirandac@hotmail.com>
2020-08-22 17:17:13 -0400

    Creates user-router.js and user-model.js

--- users/users-router.js
+++ users/users-router.js
@@ -1,2 +1,16 @@
-
 // api/users/:id 'get, put, delete' '/:id'
+
+const router = require("express").Router();
+
+const Users = require("./users-model.js");
+const protected = require("../auth/auth-middleware.js");
+
+router.get("/", protected, (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(200).json({ data: users });
+    })
+    .catch((err) => res.send(err));
+});
+
+module.exports = router;

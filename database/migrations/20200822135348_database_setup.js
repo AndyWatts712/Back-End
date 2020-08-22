1cbb922036cb0de392b9054e380c09bc8155bc4a
Shaun Lee <shaundlee.0103@gmail.com>
2020-08-22 18:27:36 -0400

    Adjusted users table

--- database/migrations/20200822135348_database_setup.js
+++ database/migrations/20200822135348_database_setup.js
@@ -3,24 +3,11 @@ exports.up = function (knex) {
     .createTable("users", (table) => {
       table.increments("id");
 
-<<<<<<< HEAD
-exports.up = function(knex) {
-    return knex.schema
-    .createTable('users', table => {
-        table.increments('id')
-
-        table.string('username', 56).notNullable()
-        table.string('email').notNullable().unique()
-        table.string('password').notNullable()
-        //Timestamps indicate when resource was created and updated
-        table.timestamps()
-=======
       table.string("username", 56).notNullable();
       table.string("email").notNullable().unique();
       table.string("password").notNullable();
       //Timestamps indicate when resource was created and updated
       table.timestamps();
->>>>>>> ef354f1921e0d590756e5d979ef9a13d9328c2bc
     })
     .createTable("listType", (table) => {
       table.increments("id");

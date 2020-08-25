dd4854a3e18b662c5dee661ee3fe9d686c48110d
Shaun Lee <shaundlee.0103@gmail.com>
2020-08-25 15:49:50 -0400

    User testing

--- users/users-router.spec.js
+++ users/users-router.spec.js
@@ -7,6 +7,10 @@ const testLogin = {
   username: "testuser1234",
   password: "testing123",
 };
+const testUpdate = {
+    username: "testuser123",
+    password: "testing123",
+  };
 
 //next batch of code is for storing token to use to access restricted
 let token;
@@ -66,12 +70,73 @@ describe("users-router", () => {
           .get("/api/users/1")
           .set("Authorization", `${token}`);
   
-        expect(res.body).toHaveProperty('username');
+        expect(res.body).toHaveProperty('data');
     });
 
     it("returns 404 if id is not in db", async () => {
         const res = await supertest(server)
           .get("/api/users/5")
+          .set("Authorization", `${token}`);
+  
+        expect(res.status).toBe(404);
+      });
+  });
+  describe("PUT /api/users/:id", () => {
+    it("returns 401 without token", async () => {
+      const res = await supertest(server)
+        .put("/api/users/1")
+        .send(testUpdate);
+
+      expect(res.status).toBe(401);
+    });
+
+    it("accesses the route with a token", async () => {
+      const res = await supertest(server)
+        .put("/api/users/1")
+        .send(testUpdate)
+        .set("Authorization", `${token}`);
+
+      expect(res.status).toBe(201);
+    });
+
+    it("returns information", async () => {
+        const res = await supertest(server)
+          .put("/api/users/1")
+          .send(testUpdate)
+          .set("Authorization", `${token}`);
+  
+        expect(res.body).toHaveProperty('username');
+    });
+
+    it("returns 404 if id is not in db", async () => {
+        const res = await supertest(server)
+          .put("/api/users/5")
+          .send(testUpdate)
+          .set("Authorization", `${token}`);
+  
+        expect(res.status).toBe(404);
+      });
+  });
+  describe("DELETE /api/users/:id", () => {
+    it("returns 401 without token", async () => {
+      const res = await supertest(server)
+        .delete("/api/users/1")
+
+      expect(res.status).toBe(401);
+    });
+
+    it("accesses the route with a token", async () => {
+      const res = await supertest(server)
+        .delete("/api/users/1")
+        .set("Authorization", `${token}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('message');
+    });
+
+    it("returns 404 if id is not in db", async () => {
+        const res = await supertest(server)
+          .delete("/api/users/5")
           .set("Authorization", `${token}`);
   
         expect(res.status).toBe(404);
